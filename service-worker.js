e1a02fdfb3a94f32d211064b74daf600922c51b7
Ice (from Travis CI) <pinky.yy.lam@gmail.com>
2020-06-21 06:47:01 +0000

    Deploy icelam/icelam.github.io to github.com/icelam/icelam.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/,/^\/data-structures-and-algorithms/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/,/^\/data-structures-and-algorithms/,/^\/px2em-converter/],
 });
